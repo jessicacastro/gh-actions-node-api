0b1f9a1c5049bae51f8a9ab051a4a4a166af1f43
Jessica Castro <jessica.castros@outlook.com>
2023-05-29 16:14:20 -0300

    Adding preview on vercel

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-import bodyParser from 'body-parser';
-import express from 'express';
+import bodyParser from "body-parser";
+import express from "express";
 
-import eventRoutes from './routes/events.js';
+import eventRoutes from "./routes/events.js";
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get("/test", (request, response) =>
+  response.json({ message: "API Works! 🚀" })
+);
 
 app.use(eventRoutes);
 
